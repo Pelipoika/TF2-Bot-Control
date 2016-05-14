575af655afba4fe8e431fcca7ef2d6fa136f9c53
JRiipinen <Pelipoika@users.noreply.github.com>
2016-05-14 16:49:47 +0300

    Add files via upload

---
+++ extension/extension.cpp
@@ -0,0 +1,62 @@
+#include "extension.h"
+#include <CDetour/detours.h>
+
+CRealizeSpyFixer g_RealizeSpyFixer;
+SMEXT_LINK(&g_RealizeSpyFixer);
+
+IGameConfig *g_pGameConf = NULL;
+
+CDetour *g_RealizeSpyDetour = NULL;
+CDetour *g_GetEventChangeAttributes = NULL;
+
+class CTFPlayer;
+
+DETOUR_DECL_MEMBER1(RealizeSpy, int, CTFPlayer *, player)
+{
+	return 0;
+}
+
+DETOUR_DECL_MEMBER1(GetEventChangeAttributes, int, char const*, attribute)
+{
+	int index = gamehelpers->EntityToBCompatRef(reinterpret_cast<CBaseEntity *>(this));
+
+	if(index > 0 && index <= playerhelpers->GetMaxClients())
+	{
+		IGamePlayer *pPlayer = playerhelpers->GetGamePlayer(index);
+		if(pPlayer->IsConnected() && pPlayer->IsInGame() && !pPlayer->IsFakeClient())
+		{
+			return 0;
+		}
+	}
+	
+	return DETOUR_MEMBER_CALL(GetEventChangeAttributes)(attribute);
+}
+
+bool CRealizeSpyFixer::SDK_OnLoad(char *error, size_t maxlength, bool late)
+{
+	if (!gameconfs->LoadGameConfigFile("bot-control", &g_pGameConf, error, maxlength)) return false;
+	
+	CDetourManager::Init(g_pSM->GetScriptingEngine(), g_pGameConf);
+	
+	if((g_RealizeSpyDetour = DETOUR_CREATE_MEMBER(RealizeSpy, "CTFBot::RealizeSpy")) != NULL)
+	{
+		g_RealizeSpyDetour->EnableDetour();
+		g_pSM->LogMessage(myself, "CTFBot::RealizeSpy detour enabled.");
+	}
+	
+	if((g_GetEventChangeAttributes = DETOUR_CREATE_MEMBER(GetEventChangeAttributes, "CTFBot::GetEventChangeAttributes")) != NULL)
+	{
+		g_GetEventChangeAttributes->EnableDetour();
+		g_pSM->LogMessage(myself, "CTFBot::GetEventChangeAttributes detour enabled.");
+	}
+	
+	return true;
+}
+
+void CRealizeSpyFixer::SDK_OnUnload()
+{
+	gameconfs->CloseGameConfigFile(g_pGameConf);
+	
+	if(g_RealizeSpyDetour != NULL) g_RealizeSpyDetour->Destroy();
+	if(g_GetEventChangeAttributes != NULL) g_GetEventChangeAttributes->Destroy();
+}
